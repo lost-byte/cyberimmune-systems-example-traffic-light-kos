8274ed924662d0ea06448e30d784e50af66b5c30
Denis <eyermac@bk.ru>
2024-02-07 14:05:42 +0300

    Запрет на два зелёных в сочетании с другими цветами

--- control_system/src/control_system.c
+++ control_system/src/control_system.c
@@ -12,7 +12,8 @@
 
 #include <assert.h>
 
-#define MODES_NUM 9
+//#define MODES_NUM 9
+#define MODES_NUM 15
 
 
 /* Control system entity entry point. */
@@ -30,7 +31,21 @@ int main(int argc, const char *argv[])
         traffic_light_IMode_Direction1Red + traffic_light_IMode_Direction2Green,
         traffic_light_IMode_Direction1Red + traffic_light_IMode_Direction2Yellow,
         traffic_light_IMode_Direction1Yellow + traffic_light_IMode_Direction1Blink + traffic_light_IMode_Direction2Yellow + traffic_light_IMode_Direction2Blink,
-        traffic_light_IMode_Direction1Green + traffic_light_IMode_Direction2Green // <-- try to forbid this via security policies
+        traffic_light_IMode_Direction1Green + traffic_light_IMode_Direction2Green, // <-- try to forbid this via security policies
+        
+        /**
+         *  Еще немного состояний для проверки
+         */
+         traffic_light_IMode_Direction1Green + traffic_light_IMode_Direction2Green + traffic_light_IMode_Direction2Red,
+         traffic_light_IMode_Direction1Green + traffic_light_IMode_Direction2Green + traffic_light_IMode_Direction1Yellow,
+         /**
+          *  И еще с мигающим
+          */
+          traffic_light_IMode_Direction1Green + traffic_light_IMode_Direction2Green + traffic_light_IMode_Direction1Blink,
+		  traffic_light_IMode_Direction1Green + traffic_light_IMode_Direction2Green + traffic_light_IMode_Direction2Blink,
+		  traffic_light_IMode_Direction1Green + traffic_light_IMode_Direction2Green + + traffic_light_IMode_Direction2Red + traffic_light_IMode_Direction1Blink,
+		  traffic_light_IMode_Direction1Green + traffic_light_IMode_Direction2Green + + traffic_light_IMode_Direction1Red + traffic_light_IMode_Direction1Blink
+
     };
 
     fprintf(stderr, "Hello I'm ControlSystem\n");
